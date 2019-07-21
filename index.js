1356551cc2897210ca4fc8d6d40d08736b576343
Mohamed Khair Dimashky <Mohamed.Khair.Dimashky@gmail.com>
2019-07-21 17:05:19 +0300

    change to the simplest method

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
-const cote = require('cote');
 const chokidar = require('chokidar');
 const fs = require("fs");
 const Path = require('path');
+const rimraf = require('rimraf');
 const moment = require('moment');
-const os = require("os");
-
 const args = process.argv.slice(2)
+const lineReader = require('line-reader');
+const destinations_file = Path.resolve("destinations.input");
+const config = require("./backup.config");
+let destination_folder_path = "";
 
-if (args.length) {
-	const client = new cote.Requester({
-		name: 'Master'
+
+const backup_folder_path = config.backup_folder;
+
+const watcher = chokidar.watch(backup_folder_path, {
+	ignoreInitial: true,
+});
+
+try {
+	watcher.on('add', (filePath) => {
+		filePath = Path.resolve(filePath);
+		console.log("file added => " + filePath + " .... waiting for 5 seconds!");
+		setTimeout(() => {
+			const filename = Path.basename(filePath);
+
+			config.destination_folders.forEach(folder => {
+				console.log("Trying to copy the file into " + folder);
+				destination_folder_path = folder;
+				try {
+					fs.copyFileSync(filePath, Path.join(destination_folder_path, filename));
+					console.log(`File ${filename} has copied to ${destination_folder_path} successfully!`);
+					deleteOldFiles(destination_folder_path, filename);
+				} catch (e) {
+					console.log("Exception while copying file to " + destination_folder_path);
+				}
+			})
+
+			deleteOldFiles(backup_folder_path, filename);
+
+		}, 5000);
 	});
+} catch (e) {
+	console.log("Exception!");
+	console.error(e);
+}
 
-	const path = args[0];
-	const watcher = chokidar.watch(path, {
-		ignoreInitial: true,
-	});
-	try {
-		watcher.on('add', (filePath) => {
-			filePath = Path.resolve(filePath);
-			console.log("file added => " + filePath + " .... waiting for 5 seconds!");
-			setTimeout(() => {
-				const file = fs.readFileSync(filePath);
-				const filename = Path.basename(filePath);
 
-				console.log("File '" + filename + "' size is " + ((file.byteLength) / 1000) + " KB");
+function deleteOldFiles(path, filename) {
+	const files = fs.readdirSync(path);
 
-				const request = {
-					type: 'backup created',
-					file,
-					from: os.hostname()
-				};
-
-				client.send(request, (res) => {
-					console.log("OK! from " + res);
-				});
-			}, 5000);
-
-		});
-	} catch (e) {
-		console.log("Exception!");
-		console.error(e);
+	if (files.length < 5) {
+		return;
 	}
-} else {
-	const BackupReciever = new cote.Responder({
-		name: 'Slave'
-	});
 
-	BackupReciever.on('backup created', (req, cb) => {
-		console.log("Recieved new backup from " + req.from);
-		const dir = `${Path.join(os.homedir(), 'Desktop', 'AlphaDental')}`;
-		if (!fs.existsSync(dir)) {
-			fs.mkdirSync(dir);
+	files.forEach(function (file) {
+		const stat = fs.statSync(Path.join(path, file));
+		const destfilename = Path.basename(file);
+
+		if (moment(stat.mtime).add(parseInt(config.delete_file_old_than), 'days').isBefore(moment()) && destfilename !== filename) {
+			return rimraf(Path.join(path, file), function (err) {
+				if (err) {
+					return console.error(err);
+				}
+				console.log('====> ' + destfilename + ' successfully deleted');
+			});
 		}
-		const path = Path.join(dir, `AD_Backup_${req.from}_${moment().toISOString().replace(/:/g,'_')}.zip`);
-		var buffer = Buffer.from(req.file.data);
-		if (req.file.data.length === 0) {
-			return;
-		}
-		fs.writeFileSync(path, buffer);
-		cb(os.hostname());
 	});
 }
